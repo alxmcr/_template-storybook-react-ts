e0e8fceb56528a72133b3a473fb6eba95b15e779
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 17:20:02 -0400

    wip

--- src/stories/components/elements/Button/index.tsx
+++ src/stories/components/elements/Button/index.tsx
@@ -9,47 +9,45 @@ export const Button = styled.button`
 `;
 
 export const DefaultButton = styled(Button)`
-  background-color: #21aa33;
-  border: 1px solid #21aa33;
-  color: #fff;
+  background-color: ${(props) => props.theme.button.solid.default.background};
+  border: 1px solid ${(props) => props.theme.button.solid.default.borderColor};
+  color: ${(props) => props.theme.button.solid.default.text};
 
   &:hover {
-    background-color: #91aa33;
-    border: 1px solid #91aa33;
-    color: #fff;
+    background-color: ${(props) => props.theme.button.solid.hover.background};
+    border: 1px solid ${(props) => props.theme.button.solid.hover.borderColor};
+    color: ${(props) => props.theme.button.solid.hover.text};
   }
 `;
 
 export const OutlineButton = styled(Button)`
-  background-color: transparent;
-  border: 1px solid #04aabb;
-  color: #04aabb;
+  background-color: ${(props) => props.theme.button.outline.default.background};
+  border: 1px solid ${(props) => props.theme.button.outline.default.borderColor};
+  color: ${(props) => props.theme.button.outline.default.text};
 
   &:hover {
-    background-color: #ee2211;
-    border: 1px solid #ee2211;
-    color: #fff;
+    background-color: ${(props) => props.theme.button.outline.hover.background};
+    border: 1px solid ${(props) => props.theme.button.outline.hover.borderColor};
+    color: ${(props) => props.theme.button.outline.hover.text};
   }
 `;
 
 export const GradientButton = styled(Button)`
   background: linear-gradient(
-    90deg,
-    rgba(2, 0, 36, 1) 0%,
-    rgba(9, 9, 121, 1) 35%,
-    rgba(0, 212, 255, 1) 100%
+    135deg,
+    ${(props) =>
+      props.theme.button.gradient.default.gradientColors.gradientStart},
+    ${(props) => props.theme.button.gradient.default.gradientColors.gradientEnd}
   );
-  border: 1px solid #54aabb;
-  color: #fff;
+  color: ${(props) => props.theme.button.gradient.default.text};
 
   &:hover {
     background: linear-gradient(
-      90deg,
-      rgba(2, 0, 36, 1) 0%,
-      rgba(9, 9, 121, 1) 35%,
-      rgba(0, 212, 255, 1) 100%
+      135deg,
+      ${(props) =>
+        props.theme.button.gradient.hover.gradientColors.gradientStart},
+      ${(props) => props.theme.button.gradient.hover.gradientColors.gradientEnd}
     );
-    border: 1px solid #54aabb;
-    color: #fff;
+    color: ${(props) => props.theme.button.gradient.hover.text};
   }
 `;
