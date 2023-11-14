9d24858f7f4b7ad8c1203875f10257c47b773eeb
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 16:23:07 -0400

    wip

--- src/pages/OnePage/index.tsx
+++ src/pages/OnePage/index.tsx
@@ -5,19 +5,28 @@ import {
 } from '../../components/elements/Button';
 import { AppNavigation } from '../../components/sections/AppNavigation';
 import './OnePage.css';
+import styled from 'styled-components';
+
+const WrapperOnePage = styled.div`
+  background-color: #fff;
+  color: #000;
+  height: 100vh;
+`;
 
 export default function OnePage() {
   return (
     <>
-      <AppNavigation />
-      <main className="one">
-        <h1 className="one__title">One</h1>
-        <div className="one__buttons">
-          <DefaultButton>Default</DefaultButton>
-          <OutlineButton>Outline</OutlineButton>
-          <GradientButton>Gradient</GradientButton>
-        </div>
-      </main>
+      <WrapperOnePage>
+        <AppNavigation />
+        <main className="one">
+          <h1 className="one__title">One</h1>
+          <div className="one__buttons">
+            <DefaultButton>Default</DefaultButton>
+            <OutlineButton>Outline</OutlineButton>
+            <GradientButton>Gradient</GradientButton>
+          </div>
+        </main>
+      </WrapperOnePage>
     </>
   );
 }
