71254bb08b480bc8c919be2688f1f92d292f81f8
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 17:26:12 -0400

    wip

--- src/stories/components/sections/AppHeader/index.tsx
+++ src/stories/components/sections/AppHeader/index.tsx
@@ -1,8 +1,10 @@
 import ThemeToggle from '../../elements/ThemeToggle';
+import { AppNavigation } from '../AppNavigation';
 
 export default function AppHeader() {
   return (
     <header className="app-header">
+      <AppNavigation />
       <ThemeToggle />
     </header>
   );
