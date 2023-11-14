cffd83e54ef426fcb845a3121f6750bc07a889b0
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 15:18:02 -0400

    wip

---
+++ src/pages/OnePage/index.tsx
@@ -0,0 +1,7 @@
+export default function OnePage() {
+  return (
+    <main className="one">
+      <h1 className="one__title">One</h1>
+    </main>
+  );
+}
---
+++ src/pages/TwoPage/index.tsx
@@ -0,0 +1,7 @@
+export default function TwoPage() {
+  return (
+    <main className="two">
+      <h1 className="two__title">Two</h1>
+    </main>
+  );
+}
