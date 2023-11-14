6a22170192f22a5a7e3029d981545d88b7996413
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 15:30:53 -0400

    wip

---
+++ src/components/sections/AppNavigation/index.tsx
@@ -0,0 +1,11 @@
+export const AppNavigation = () => {
+  return (
+    <nav className="app-navigation">
+      <ul className="app-navigation__list">
+        <li className="app-navigation__item">
+          <a href="" className="app-navigation__link"></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
