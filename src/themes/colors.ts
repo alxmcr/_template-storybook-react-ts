36ce52e698df0c30cf0398767a66552b1f2f31e7
Alejandro Coca <alxmcr@outlook.com>
2023-11-15 18:53:11 -0400

    wip

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -14,4 +14,18 @@ const blue = {
   950: '#240077',
 };
 
-export const colors = { light, blue };
+const grape = {
+  50: '#f6f5fd',
+  100: '#f0ecfb',
+  200: '#e3dcf8',
+  300: '#cdc0f2',
+  400: '#b39ce9',
+  500: '#9a74de',
+  600: '#8a55d2',
+  700: '#7a43be',
+  800: '#66389f',
+  900: '#552f83',
+  950: '#351d58',
+};
+
+export const colors = { light, blue, grape };
