6bd8de1ed5a1fbc32421b470ff613b2a178bdce5
Alejandro Coca <alxmcr@outlook.com>
2023-11-15 12:54:47 -0400

    wip

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,15 @@
+import React from 'react';
+import StyledThemeProvider from '../src/providers/StyledThemeProvider';
 import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <StyledThemeProvider>
+        <Story />
+      </StyledThemeProvider>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
