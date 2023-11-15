f254d0b797e5b00f1742ec1bf43045aad877c553
Alejandro M. Coca <alxmcr@outlook.com>
2023-11-14 21:07:29 -0400

    wpi

---
+++ src/stories/components/buttons/ButtonDefault.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ButtonDefault } from './ButtonDefault';
+
+const meta = {
+  title: 'atoms/buttons/ButtonDefault',
+  component: ButtonDefault,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ButtonDefault>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
