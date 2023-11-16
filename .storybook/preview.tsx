f99ce4bfe42a845c0ac5b3e17ba2ef745563f166
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2023-11-16 12:05:25 -0400

    Feature/05 theming on storybook (#5)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import StyledThemeProvider from '../src/providers/StyledThemeProvider';
 import type { Preview } from '@storybook/react';
+
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { themes } from '@storybook/theming';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+/* TODO: update import for your custom theme configurations */
+import { darkTheme, lightTheme } from '../src/themes';
+
+/* TODO: replace with your own global styles, or remove */
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  `;
 
 const preview: Preview = {
   decorators: [
-    (Story) => (
-      <StyledThemeProvider>
-        <Story />
-      </StyledThemeProvider>
-    ),
+    withThemeFromJSXProvider({
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'dark',
+      Provider: ThemeProvider,
+      GlobalStyles,
+    }),
   ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -17,6 +33,10 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      autodocs: 'tag',
+      theme: themes.dark,
     },
   },
 };
